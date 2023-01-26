939c9b180c5006a1e6a6b9682896ef3d6c3899b3
paigupai <44311361+paigupai@users.noreply.github.com>
2023-01-26 22:37:19 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # unblock_bilibili
+
+[谷歌云版](https://github.com/Admirepowered/bilibli-proxy)  
+[aliyun版](https://github.com/zzc10086/grocery_store/blob/master/bili_proxy/aliyun_Serverless_BPplayurl.php)  
--- aliyun_Serverless_BPplayurl_Master.php
+++ aliyun_Serverless_BPplayurl_Master.php
@@ -1,4 +1,4 @@
-y<?php
+<?php
 
 use RingCentral\Psr7\Response;
 /*
@@ -21,43 +21,63 @@ function handler($request, $context): Response
     $clientIP   = $request->getAttribute('clientIP');
     */
     /* Config */
-
     $upstream_pc_url = 'https://api.bilibili.com/pgc/player/web/playurl';
     $upstream_app_url = 'https://api.bilibili.com/pgc/player/api/playurl';
+    $upstream_pc_search_url = 'https://api.bilibili.com/x/web-interface/search/type';
     $timeout = 5; // seconds
-    // 地区不可见code
-    $area_limit_code = -10403;
-    // 中转地址
-    $transfer_address = '';
+
 
     /* Read incoming request */
     $request_method = $request->getMethod();
-    $request_query = stristr($request->getAttribute("requestURI"), '?');
-    $req_referer = $request->getHeaderLine('referer');;
+    $queries  = $request->getQueryParams();
+    $request_query = substr(stristr($request->getAttribute("requestURI"), '?'),1);
+    //$request->getHeaderLine('referer')会被阿里云替换成云函url
+    //$req_referer = $request->getHeaderLine('referer');
+    $req_referer = "https://www.bilibili.com";
     $request_headers = $request->getHeaders();
     $request_body = $request->getBody()->getContents();
+    $request_uri = $request->getAttribute('requestURI');
+
 
 
     /* Forward request */
     $ch = curl_init();
 
-    //清理相关header
-    array_splice($request_headers, array_search('HOST', $request_headers));
-    array_splice($request_headers, array_search('User-Agent', $request_headers));
-    array_splice($request_headers, array_search('Referer', $request_headers));
-
+    //处理请求相关header
+    $request_headers = array_remove_by_key($request_headers,'X-Forwarded-Proto');
+    $request_headers = array_remove_by_key($request_headers,'Host');
+    $request_headers = array_remove_by_key($request_headers,'Referer');
+    //配置body压缩方式
+    $request_headers = array_remove_by_key($request_headers,'Accept-Encoding');
+    curl_setopt($ch, CURLOPT_ENCODING, "identity");//好像b站只有br压缩
 
     $headers = array();
     foreach ($request_headers as $key => $value) {
-        $headers[] = $key . ': ' . $value;
+        $headers[] = $key . ": " .implode($value);
     }
-    //判断使用pc还是app接口
-    if (substr_count($request_query, 'platform=android') != 0) {
-        $url = $upstream_app_url . $request_query;
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Bilibili Freedoooooom/MarkII');
-    } else {
-        $url = $upstream_pc_url . $request_query;
+
+    //判断请求接口
+    if(substr_count($request_uri,'/search/type')!=0){
+        //搜索接口强制需要buvid3这个cookie,应该与账号无关,访问https://api.bilibili.com/x/frontend/finger/spi可直接获取
+        if (substr_count($request_query,'buvid3')!=0) array_push($headers,"cookie: buvid3=".$queries['buvid3']);
+        $url = $upstream_pc_search_url . '?' .$request_query;
         curl_setopt($ch, CURLOPT_REFERER, $req_referer);
+    }elseif (substr_count($request_uri,'playurl')!=0){
+        //判断使用的那个pc还是app接口
+        if(substr_count($request_query,'platform=android')!=0){
+            $url = $upstream_app_url . '?' .$request_query;
+            curl_setopt($ch, CURLOPT_USERAGENT, 'Bilibili Freedoooooom/MarkII');
+        }else{
+            $url = $upstream_pc_url . '?' .$request_query;
+            curl_setopt($ch, CURLOPT_REFERER, $req_referer);
+        }
+    }else{
+        $header['Content-Type'] = 'text/plain';
+        return new Response(
+            502,
+            $header,
+            'Failed to match interface.'
+        );
     }
     //curl配置
     curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
@@ -71,7 +91,7 @@ function handler($request, $context): Response
 
     $response = curl_exec($ch);
     $header = array();
-  
+
     if ($response === false) {
         $header['Content-Type'] = 'text/plain';
         return new Response(
@@ -80,66 +100,69 @@ function handler($request, $context): Response
             'Upstream host did not respond.'
         );
     } else {
-
         $header_length = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-        $curl_response_headers = explode("\n", substr($response, 0, $header_length));
+        $response_headers = explode("\n", substr($response, 0, $header_length));
         $response_body = substr($response, $header_length);
-
-        $body_string = json_decode($response_body, true);
-        $status_code = $body_string['code'];
-        curl_close($ch);
-  // 地区限制，转发香港
-    if ($status_code == $area_limit_code) {
-
-    $ch_HK = curl_init();
-
-    $url_HK = $transfer_address . $request_query;
-    curl_setopt($ch_HK, CURLOPT_TIMEOUT, $timeout);
-    curl_setopt($ch_HK, CURLOPT_CUSTOMREQUEST, $request_method);
-    curl_setopt($ch_HK, CURLOPT_URL, $url_HK);
-    curl_setopt($ch_HK, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch_HK, CURLOPT_POSTFIELDS, $request_body);
-    curl_setopt($ch_HK, CURLOPT_HEADER, true);
-    curl_setopt($ch_HK, CURLOPT_FOLLOWLOCATION, false);
-    curl_setopt($ch_HK, CURLOPT_RETURNTRANSFER, true);
-    $response = curl_exec($ch_HK);
-
-    $header_length = curl_getinfo($ch_HK, CURLINFO_HEADER_SIZE);
-    $curl_response_headers = explode("\n", substr($response, 0, $header_length));
-    $response_body = substr($response, $header_length);
-    curl_close($ch_HK);
-
-    foreach ($curl_response_headers as $header_string) {
-        $header_tmp = explode(': ', $header_string, 2);
-        if (count($header_tmp) == 2) {
-            // 去除多余的Access-Control-Expose-Headers
-            if ($header_tmp[0] == 'Access-Control-Expose-Headers') {
-                continue;
+        //处理返回相关header
+        foreach ($response_headers as $n => $response_header) {
+            //配置返回的body压缩方式
+            if (strpos($response_header, "Content-Encoding") !== false) {
+                $response_headers[$n] = "Content-Encoding: identity\n";
             }
-            $header[$header_tmp[0]] = trim($header_tmp[1]);
+            //删除B站返回的Content-Length,防止浏览器只读取Content-Length长度的数据,造成json不完整
+            if (strpos($response_header, "Content-Length") !== false) {
+                unset($response_headers[$n]);
+            }
+            //阿里云函数好像会自己添加Access-Control-Allow-Credentials头，删除b站返回的
+            if (strpos($response_header, "Access-Control-Allow-Credentials") !== false) {
+                unset($response_headers[$n]);
+            }
         }
-    }
+        unset($response_header); 
         
-    return new Response(
-        200,
-        $header,
-        $response_body
-    );
-    }
-
-        foreach ($curl_response_headers as $header_string) {
+        //response_headers数组转成key=>value形式
+        foreach ($response_headers as $header_string) {
             $header_tmp = explode(': ', $header_string, 2);
             if (count($header_tmp) == 2) {
                 $header[$header_tmp[0]] = trim($header_tmp[1]);
             }
         }
 
+        curl_close($ch);
         // 这行用于调试请求信息
-        //return new Response(200, array(), json_encode(array('header' => $header, 'body' => $response_body, 'url' => $url, 'response'=>$response, 'curl_headers'=>$curl_response_headers, 'status_code'=>$status_code)));
+        // return new Response(200, array(), json_encode(array('header' => $header, 'body' => $response_body, 'url' => $url, 'response'=>$response, 'curl_headers'=>$curl_response_headers)));
         return new Response(
             200,
             $header,
             $response_body
         );
     }
+}
+
+/*tool*/
+//某个字符串在另一个字符串第N此出现的下标
+function str_n_pos($str, $find, $n)
+{
+    $pos_val = 0;
+    for ($i = 1; $i <= $n; $i++) {
+        $pos = strpos($str, $find);
+        $str = substr($str, $pos + 1);
+        $pos_val = $pos + $pos_val + 1;
+    }
+    $count = $pos_val - 1;
+    return $count;
+}
+
+function array_remove_by_key($arr, $key)
+{
+	if(!array_key_exists($key, $arr)){
+		return $arr;
+	}
+	$keys = array_keys($arr);
+	$index = array_search($key, $keys);
+	if($index !== FALSE){
+		array_splice($arr, $index, 1);
+	}
+
+	return $arr;
 }
